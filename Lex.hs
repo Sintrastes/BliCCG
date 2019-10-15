55239b6e0aca3e824658617806f6486b106d8570
Nathan Bedell <nbedell@tulane.edu>
2019-10-15 11:04:05 -0400

    Added helper function for displaying function types.

--- Lex.hs
+++ Lex.hs
@@ -2,9 +2,11 @@ module Lex (Prim(..),
             Slash(..),
             Cmplx(..),
             Lexicon,
-            createLexicon) where
+            createLexicon,
+            toFunc) where
 
 import qualified Data.Map as Map
+import Data.List
 
 type Prim  = String
 data Slash = Forw | Back deriving (Eq, Ord)
@@ -19,6 +21,20 @@ data Cmplx = CmplxLeaf Prim |
 instance Show Cmplx where
     show (CmplxLeaf a)     = show a
     show (CmplxTree a b c) = "(" ++ show a ++ show b ++ show c ++ ")"
+
+newtype Func = Func [Prim]
+
+instance Show Func where
+  show (Func xs) = intercalate " -> " xs ++ "." 
+
+-- Helper function to extract what a Cmplx looks like
+-- as a function. Extracts a list of arguments in the order
+-- they are expected.
+toFunc :: Cmplx -> Func
+toFunc (CmplxLeaf x) = Func [x]
+toFunc (CmplxTree a _ b) = 
+  let (Func b', Func a') = (toFunc b, toFunc a) 
+  in Func $ b' ++ a'
 
 type Lexicon = Map.Map String [Cmplx]
 
